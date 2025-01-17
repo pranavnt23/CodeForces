d5ebc58f47d222db524edc9077963fc35fc5e0a5
pranavnt23 <123755545+pranavnt23@users.noreply.github.com>
2025-01-17 19:35:49 +0530

    Create 5. Dislike of Threes.cpp

---
+++ 800/5. Dislike of Threes.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int tests;cin>>tests;
+    while(tests--){
+        int k;cin>>k;
+        for(int i=1;;i++){
+            if(i%3==0 || i%10==3){
+                continue;
+            }
+            else{
+                --k;
+                if(k==0){cout<<i<<endl;break;}
+            }
+        }
+    }
+}
